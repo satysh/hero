cbc5aadd89c435e26a218adfa13d30a9cb8e9f67
Ilyas <satyshevi@gmail.com>
2019-07-08 23:42:50 +0300

    diff Ions run sim (proton, alpha, c, ferum with Ekin=13. GeV)

--- macro/prototype/sim.C
+++ macro/prototype/sim.C
@@ -19,14 +19,21 @@
 int  GetPdgCode(const int Z, const int A);
 void AddIon(const int pdg);                    //For PDG ion beam
 
-void sim(Int_t nEvents = 1, Int_t index = 0, TString outDir = "output")
+void sim(Int_t nEvents = 1, Int_t index = 0, TString outDir = "output", Int_t IonIndex = 1)
 {
 
   // -----   Particle  --------------------------------------------------------
   Int_t pdgId = 2212; // proton 2212 // electron 11
-  Double32_t momentum = 300.; // GeV
-
-  pdgId = GetPdgCode(26,56);      // Set nuclear pdg for Ion
+  Double32_t momentum = 13.; // GeV
+  Int_t curZ, curA;
+  switch(IonIndex) {
+    case 1 : curZ = 1; curA = 1; break;
+    case 2 : curZ = 2; curA = 4; break;
+    case 3 : curZ = 6; curA = 12; break;
+    case 4 : curZ = 26; curA = 56; break;
+    default : cerr << "index error" << endl; return ;
+  }
+  pdgId = GetPdgCode(curZ,curA);      // Set nuclear pdg for Ion
 
   // ------------------------------------------------------------------------
 
@@ -69,7 +76,8 @@ void sim(Int_t nEvents = 1, Int_t index = 0, TString outDir = "output")
 // -----   Create PrimaryGenerator   --------------------------------------
   FairPrimaryGenerator* primGen = new FairPrimaryGenerator();
   FairBoxGenerator* boxGen = new FairBoxGenerator(pdgId, 1);
-  boxGen->SetPRange(momentum, momentum);
+  //boxGen->SetPRange(momentum, momentum);
+  boxGen->SetEkinRange(momentum, momentum);
   boxGen->SetThetaRange(0., 0.); // 0-90
   boxGen->SetPhiRange(0., 0.); // 0-360
   boxGen->SetBoxXYZ(-4., 3., -4., 3., -500.); // xmin, ymin, xmax, ymax, z
@@ -82,7 +90,7 @@ void sim(Int_t nEvents = 1, Int_t index = 0, TString outDir = "output")
   primGen->AddGenerator(boxGen);
   run->SetGenerator(primGen);
 
-  run->SetStoreTraj(kTRUE); // kFALSE
+  run->SetStoreTraj(kFALSE); // kFALSE
 
   //-------Set LOG verbosity  -----------------------------------------------
   FairLogger::GetLogger()->SetLogVerbosityLevel("LOW");
